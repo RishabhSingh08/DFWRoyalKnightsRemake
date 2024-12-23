2fe6e3ec16d7102058d0798117baffd5202f5297
Rishabh Singh <Rishabh.home.1@gmail.com>
2024-12-23 01:15:31 -0600

    commit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,27 @@
 <script>
 	import '../app.css';
+	import Logo from "../lib/assets/Logo.png";
 	let { children } = $props();
 </script>
 
 <main>
-	
-	{@render children()}
+<nav class="px-32 py-4 fixed bg-black w-full z-10">
+  <div class="flex items-center justify-between">
+	<a href="/">
+		<img src={Logo} alt="Logo" class="w-12">
+	</a>
 
+    <ul class="flex space-x-12">
+      <li><a href="/events" class="text-xl hover:text-gray-500">Events</a></li>
+      <li><a href="/team" class="text-xl hover:text-gray-500">Our Team</a></li>
+      <li><a href="/contact" class="text-xl hover:text-gray-500">Contact Us</a></li>
+      <li><a href="/donate" class="text-xl hover:text-gray-500">Donate</a></li>
+    </ul>
+  </div>
+</nav>
+	{@render children()}
+	<footer>
+		Hello
+	</footer>
 </main>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script>
-    import Logo from "../lib/assets/Logo.png";
     import LogoExtended from "../lib/assets/LogoExtended.png";
     import PickleballLogo from "../lib/assets/PickleballLogo.png";
     import SistersLogo from "../lib/assets/SistersLogo.png";
@@ -50,34 +49,33 @@
 
 </style>
 <main>
-    <div class="poppins bg-center bg-cover grid grid-cols-5"
+    <div class="poppins bg-center bg-cover grid "
         style={`background-image: url(${BackgroundImage});`}>
 
-        <div class="col-span-1"></div>
-        <div class="col-span-3 mt-32 mb-32">
+        <div class="2xl:my-32 mt-32 mb-16 mx-32 2xl:mx-54">
             <!-- Hero Text -->
-            <div class="mb-24 grid grid-cols-5 items-center justify-center">
-                <div class="border-r col-span-3">
-                    <img src={LogoExtended} alt="Logo" class="my-8">
-                    <p class="text-2xl w-[40rem]">Building a community of strategic thinkers, skilled players, and passionate chess enthusiasts</p>
+            <div class="mb-16 grid 2xl:grid-cols-5 grid-cols-7 items-center justify-center ">
+                <div class="border-r 2xl:col-span-3 col-span-4 ">
+                    <img src={LogoExtended} alt="Logo" class="my-8 pr-36">
+                    <p class="text-2xl 2xl:w-[40rem]">Building a community of strategic thinkers, skilled players, and passionate chess enthusiasts</p>
                     <button class="border px-8 text-xl py-3 my-8 rounded-md border-[#ff9900] transition hover:text-[#ff9900] hover:bg-white hover:border-white">
-                        <a href="https://www.discord.com">Join Us</a>
+                        <a href="https://discord.gg/h5ZRQVA4jQ" target="_blank">Join Us</a>
                     </button>
                 </div>
-                <div class="text-center w-full col-span-2">
-                    <p class="text-5xl mb-5">
+                <div class="text-center w-full 2xl:col-span-2 col-span-3 ">
+                    <p class="2xl:text-5xl mb-5 text-4xl">
                         Cultivating
                     </p>
-                    <span class="poppins-bold text-7xl">{currentWord}</span>
-                    <p class="text-4xl mt-5">One Move at a Time</p>
+                    <span class="poppins-bold 2xl:text-6xl text-5xl">{currentWord}</span>
+                    <p class=" mt-5 text-3xl 2xl:text-4xl">One Move at a Time</p>
                 </div>
             </div>
             <!-- Sponsors -->
-            <div class="mt-16">
+            <div>
                 <div class="mb-8">
                     <p class="text-2xl text-center text-zinc-300">Sponsored By</p>
                 </div>
-                <div class="grid grid-cols-2 mx-96 text-7xl">
+                <div class="grid grid-cols-2 2xl:mx-96 mx-64 text-7xl">
                     <div class="flex justify-center items-center">
                         <a href="https://propickleballessentials.jimdosite.com/" target="_blank">
                             <img src={PickleballLogo} alt="Pickleball Logo" class="w-20 h-auto">
@@ -92,20 +90,18 @@
                 </div>
             </div>
         </div>
-        <div class="col-span-1"></div>
     </div>
 
-    <div class="grid grid-cols-5">
+    <div class="grid 2xl:grid-cols-7 grid-cols-7">
         <div class="col-span-1"></div>
         <!-- Content Div -->
-         
-        <div class="col-span-3">
-                        <div class="text-center poppins mt-16 mb-32">
+        <div class="2xl:col-span-5 col-span-5">
+            <div class="text-center poppins my-16">
                 <div>
                     <p class="text-4xl mb-4">Mission Statement</p>
                 </div>
-                <div class=" mx-32">
-                    <p class=" ">
+                <div>
+                    <p>
                         Our mission is to promote chess in underserved communities and provide a free, joyful, and educational medium for tutoring kids in the strategic game of chess while fostering a love for the game through engaging chess events.
                     </p>
                 </div>
@@ -113,9 +109,9 @@
 
             <!-- About Us -->
             <div class="">
-                <div class="flex justify-center items-center gap-16">
-                    <div class="text-center col-span-2">
-                        <p class="text-4xl poppins-light">At DFW Royal Knights, we aim to inspire the next generation of strategic thinkers and chess enthusiasts by fostering a passion for the game and creating opportunities for students to connect, learn, and grow in a supportive community.</p>
+                <div class="grid grid-cols-3 justify-center items-center gap-16">
+                    <div class="text-center">
+                        <p class="text-xl 2xl:text-2xl poppins-light">At DFW Royal Knights, we aim to inspire the next generation of strategic thinkers and chess enthusiasts by fostering a passion for the game and creating opportunities for students to connect, learn, and grow in a supportive community.</p>
                         <!-- <div class="flex justify-center gap-16 text-center text-5xl my-12">
                             <div><div>6.6k+</div><p class="mt-2 text-base">People affected</p></div>
                             <div><div>$3.4k+</div><p class="mt-2 text-base">Amount raised</p></div>
@@ -123,50 +119,44 @@
                             <div><div>11+</div><p class="mt-2 text-base">Programs</p></div>
                         </div> -->
                     </div>
-                    <div>
-                        <img src={AboutUsImage} alt="Who We Are" class="rounded-3xl justify-end">
+                    <div class="col-span-2">
+                        <img src={AboutUsImage} alt="Who We Are" class="rounded-3xl">
                     </div>
                 </div>
             </div>
             
             <div class="border-b my-12"></div>
-            
             <div>
                 <div class="text-center text-4xl my-12">
                     <p class="">Our Services</p>
                 </div>
-                <div class="grid grid-cols-5 items-center mx-44 mb-32 gap-16">
-                    <div class="col-span-3">
-                        <img src={GroupLessonsImage} alt="Group Lesson" class="rounded-xl">
+                <div class="grid grid-cols-5 items-center">
+                    <div class="col-span-3 ">
+                        <img src={GroupLessonsImage} alt="Group Lesson" class="rounded-xl scale-75">
                     </div>
-                    <div class="col-span-2 w-auto">
+                    <div class="col-span-2  w-auto">
                         <p  class="text-3xl mb-2 font-semibold">Group Lessons</p>
                         <p class="text-2xl" >Chess players of similar levels learn from a tutor through lessons, scrimmages, and staff appreciation events.</p>
                     </div>
                 </div>
-                                <div class="grid grid-cols-5 items-center mx-44 mb-32 gap-16">
-
+                <div class="grid grid-cols-5 items-center text-right">
                     <div class="col-span-2">
                         <p  class="text-3xl mb-2 font-semibold">Private Lessons</p>
                         <p class="text-2xl" >Personalized one-on-one lessons with experienced tutors to improve strategic thinking and gameplay techniques.</p>
                     </div>
-
                     <div class="col-span-3">
-                        <img src={PrivateLessonsImage} alt="Group Lesson" class="rounded-xl">
+                        <img src={PrivateLessonsImage} alt="Group Lesson" class="rounded-xl scale-75">
                     </div>
-
                 </div>
-                <div class="grid grid-cols-5 items-center mx-44 mb-32 gap-16">
+                <div class="grid grid-cols-5 items-center">
                     <div class="col-span-3">
-                        <img src={EventsImage} alt="Group Lesson" class="rounded-xl">
+                        <img src={EventsImage} alt="Group Lesson" class="rounded-xl scale-75">
                     </div>
                     <div class="col-span-2 w-auto">
                         <p  class="text-3xl mb-2 font-semibold">Events</p>
-                        <p class="text-2xl" >Tournaments, scrimmages, online sessions, and chess parties for competitive play, skill testing, and social enjoyment.
-</p>
+                        <p class="text-2xl" >Tournaments, scrimmages, online sessions, and chess parties for competitive play, skill testing, and social enjoyment.</p>
                     </div>
                 </div>
-
             </div>
             
         </div>
